22e0b2214a0d4db6e05c120abcebcd4f2ed37bea
Jeremy Johnson <jmjohnson@ci.davenport.ia.us>
2019-01-02 08:02:51 -0600

    updated vars for chocolatey install

--- tools/vars.ps1
+++ tools/vars.ps1
@@ -1,5 +1,6 @@
 $AppName = "ADUpdate"
-$RepoName = "it-adupdate"
+$RepoName = "it-powershell-adupdate"
 $SourceDir = "C:\ProgramData\chocolatey\lib\{0}" -f $RepoName
-$DestinationDir = "C:\Windows\SYSVOL\domain\scripts\{0}" -f $AppName
-[String[]] $ExecutablesToIgnore = "$SourceDir\$AppName.exe" #, $SourceDir\<NextExecutableInArray>.exe
+$DestinationDir = "C:\Test\{0}" -f $AppName
+#$DestinationDir = "C:\Windows\SYSVOL\domain\scripts\{0}" -f $AppName
+[String[]] $ExecutablesToIgnore = @{}#"$SourceDir\$AppName.exe" #, $SourceDir\<NextExecutableInArray>.exe
